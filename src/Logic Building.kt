20bf430fdd8adcc20fdead2934b4a53710c770f8
Muhammad Ismail <226304039+Muhammad-Ismail124@users.noreply.github.com>
2025-12-10 15:53:41 +0500

    Added more functions

--- src/Logic Building.kt
+++ src/Logic Building.kt
@@ -1,6 +1,7 @@
 import kotlin.math.pow
 import kotlin.math.sqrt
 
+//---------------------------------------------------------------------------------------------------
 fun evenOrOdd(n: Int) {
     if (n.isEven()) {
         println("Even")
@@ -9,8 +10,10 @@ fun evenOrOdd(n: Int) {
     }
 }
 
+//-------------------------------------------------------------------------------------------------------
 fun Int.isEven(): Boolean = this % 2 == 0
 
+//------------------------------------------------------------------------------------------------------------
 fun multiTable() {
     println("Enter The Number: ")
     val n = readln().toInt()
@@ -20,6 +23,7 @@ fun multiTable() {
     }
 }
 
+//-------------------------------------------------------------------------------------------------------------------------
 fun sumOfNaturals() {
 
     println("Enter A number: ")
@@ -31,6 +35,7 @@ fun sumOfNaturals() {
     println("$sum")
 }
 
+//------------------------------------------------------------------------------------------------------
 fun squareOfNaturals() {
     println("Enter A number: ")
     val num = readln().toInt()
@@ -42,6 +47,7 @@ fun squareOfNaturals() {
     print("$sum")
 }
 
+//--------------------------------------------------------------------------------------------------------
 fun swapTwoNum() {
 
     println("Enter A number: ")
@@ -55,6 +61,7 @@ fun swapTwoNum() {
     println("First Place: $num1   Second Place:  $num2  Boom Numbers Swapped Magic ")
 }
 
+//----------------------------------------------------------------------------------------------------------
 fun swapNumbers() {
 
     println("Enter A number: ")
@@ -70,6 +77,7 @@ fun swapNumbers() {
     println("First Place: $num1   Second Place:  $num2  Boom Numbers Swapped Magic ")
 }
 
+//--------------------------------------------------------------------------------------------------------
 fun oppositeOfFaceDice() {
 
     println("Enter A Number to check on the dice: ")
@@ -107,11 +115,13 @@ fun oppositeOfFaceDice() {
     }
 }
 
+//------------------------------------------------------------------------------------------------------------
 fun nthTermFromFirstTwoTerms(a1: Int, a2: Int, n: Int): Int {
     return (a1 + (n - 1) * (a2 - a1))
 
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun sumOfIntegers() {
     println("Enter some numbers: ")
     val nums = readln().toInt()
@@ -124,6 +134,7 @@ fun sumOfIntegers() {
     println("The Sum of digits $nums is: $sum")
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun reverseNumbers() {
     println("Enter Some Integers: ")
     var nums = readln().toInt()
@@ -135,6 +146,7 @@ fun reverseNumbers() {
     println("The revNums is $revNums")
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun checkPowerOfNumber() {
     println("Enter Base Number: ")
     val base = readln().toInt()
@@ -159,6 +171,7 @@ fun checkPowerOfNumber() {
 
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun calculatePower(base: Int, exponent: Int): Int {
     var result = 1
     for (i in 1..exponent) {
@@ -167,18 +180,21 @@ fun calculatePower(base: Int, exponent: Int): Int {
     return result
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun distanceBetweenTwoNumbers(x1: Int, y1: Int, x2: Int, y2: Int): Double {
     val y3 = calculatePower(y2 - y1, 2)
     val x3 = calculatePower(x2 - x1, 2)
     return sqrt((x3 + y3).toDouble())
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun calculatePie() {
     println("Pie is the division of 22/7")
     val pie = 22F / 7F
     println("So the value of pie is $pie")
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 fun validTriangle(a: Int, b: Int, c: Int) {
 
     if (a + b > c && a + c > b && b + c > a) {
@@ -189,34 +205,170 @@ fun validTriangle(a: Int, b: Int, c: Int) {
 
 }
 
-fun calcVolumeOfSphere(){
+//----------------------------------------------------------------------------------------------------------------------
+fun calcVolumeOfSphere() {
     println("Enter Radius of Sphere: ")
     val radius = readln().toFloat()
-    val pie = 22F/7F
+    val pie = 22F / 7F
     val volume = 0.75 * pie * radius.pow(3)
 
     println("The Volume Of Sphere With A Radius Of $radius is $volume")
 }
 
-fun nameUpperCaseReverse(){
-     println("Enter Your Name: ")
-    val input  = readln()
+//----------------------------------------------------------------------------------------------------------------------
+fun nameUpperCaseReverse() {
+    println("Enter Your Name: ")
+    val input = readln()
     val string = input[0].uppercase() + input.substring(1, input.length)
     println(reverseString(string))
 }
+
+//----------------------------------------------------------------------------------------------------------------------
 fun reverseString(name: String): String {
 
     var reverse = ""
 
-    for (i in name.length -1 downTo 0) {
+    for (i in name.length - 1 downTo 0) {
 
         reverse += name[i]
     }
     return reverse
 }
 
+//----------------------------------------------------------------------------------------------------------------------
+fun ifPalindrome() {
+    println("Enter A Word To Check Palindrome")
+    val palindrome = readln()
 
-//fun rectangleOverlap(topLeft: Pair<Int, Int>, l2: Pair<Int, Int>, r1: Pair<Int, Int>, r2: Pair<Int, Int>) {
-//
-//    if (topLeft.first > r1.first ||)
-//}
+    val answer = if (palindrome == palindrome.reversed()) "Palindrome" else "Not Palindrome"
+
+    val uppercase = palindrome[0].uppercase() + palindrome.substring(1)
+
+    println("The Word you entered is $uppercase which is $answer")
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+fun ageReadline() {
+
+    println("Enter Your Age: ")
+    val age: Int? = readlnOrNull()?.toInt()
+
+    if (age != null) {
+        when {
+            age < 18 -> println("You Are Not An Adult")
+            age in 18..60 -> println("You Are An Adult")
+            else -> println("You Are really old")
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+fun ageReadline2() {
+    println("Enter Your Age: ")
+    val age: Int? = readlnOrNull()?.toInt()
+
+    if (age != null) {
+        if (age < 18) {
+            println("You are not an adult")
+        } else if (age in 18..60) {
+            println("You Are An Adult")
+        } else {
+            println("You Are really old")
+
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+fun tillZero() {
+    println("Enter A Number: ")
+    var num = readln().toInt()
+
+    while (num != 0) {
+        println(num)
+        num--
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+fun toThePowerOf() {
+    println("Enter number 1 : ")
+    val base = readln().toInt()
+    println("Enter number 2 : ")
+    val exp = readln().toInt()
+    var answer = 1
+    for (i in 1..exp) {
+        answer *= base
+    }
+    println("$base to the power of $exp is $answer")
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+fun arrayActions() {
+
+    println("Enter Numbers one by one And Type 'done' to finish ")
+
+    var myArray = intArrayOf()
+
+    while (true) {
+
+        val input = readln()
+
+        if (input == "Done" || input == "done") {
+
+            break
+        }
+        val number = input.toIntOrNull()
+
+        if (number != null) {
+            myArray += number
+        } else {
+            println("Please A valid number or 'done' to finish")
+        }
+    }
+    println("The Array Contents Are : ${myArray.joinToString(", ")}")
+
+    println("Enter An Action: Reverse Or Start Or sum to perform action on Array: ")
+
+    val answer = readlnOrNull().toString()
+
+    var size = myArray.size - 1
+
+    if (answer == "Reverse" || answer == "reverse") {
+
+        print("Reverse: ${myArray.reversed().joinToString(", ")}")
+    }
+
+
+    if (answer == "start" || answer == "Start") {
+
+        print("From The Start The Array Has: ${myArray.joinToString(", ")}")
+
+    }
+
+    var sum = 0
+
+    if (answer == "sum" || answer == "Sum") {
+        for (i in myArray) {
+            sum += i
+        }
+
+        println("The Sum Of Contents Of Numbers ${myArray.joinToString(",")} Is: $sum")
+    }
+
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+
+/*
+simple expression = 3 > 4 || 4 > 3 && 4 <= 4
+x = 9     ,   y = 3     ,     z= 9
+hard Expression = !(x != z) && bool || z > (x + y) && (!bool || y > z)
+                  !(false) && true || 9 > (9+3) && (!true || 3 > 9)
+! = converts into false into true or true into false
+                  true && true || 9 > 12 -> false && (false || false)
+                  true || false && false
+                  true || false
+                  -> True
+ */
